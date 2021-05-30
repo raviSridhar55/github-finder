04ee44766a0a1114044eb9ec76f7dadade8bfaf8
raviSridhar55 <61363065+raviSridhar55@users.noreply.github.com>
2021-05-30 14:07:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Layout/Navbar';
+// import Body from './Components/Layout/Body';
+import Alert from './Components/Layout/Alert';
+import Users from './Components/users/Users';
+import Search from './Components/users/Search';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data.items, loading: false });
+  // }
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear USers
+  clearUsers = () => this.setState({ users: [], loading: false });
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar title='GitHub Finder' icon='fab fa-github' />
+
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Layout/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <div className='bg-secondary'>
+      <div className='container'>
+        <h1 className='text-center'>Hey You!!</h1>
+        <p className='lead text-center'>Wanna Seacrh For In GitHub??</p>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
